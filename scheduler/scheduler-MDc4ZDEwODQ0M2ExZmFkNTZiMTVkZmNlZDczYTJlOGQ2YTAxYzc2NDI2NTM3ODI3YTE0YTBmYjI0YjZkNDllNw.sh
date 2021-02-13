3ea08060eafd27bbe10b5b2d8022a540840c8b67
noplanalderson <noplanalderson@gmail.com>
2021-02-13 20:40:06 +0700

    Delete Cron Script Sample

--- scheduler/scheduler-MDc4ZDEwODQ0M2ExZmFkNTZiMTVkZmNlZDczYTJlOGQ2YTAxYzc2NDI2NTM3ODI3YTE0YTBmYjI0YjZkNDllNw.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/curl -k 'https://localhost/suhu-app/get-suhu/MDc4ZDEwODQ0M2ExZmFkNTZiMTVkZmNlZDczYTJlOGQ2YTAxYzc2NDI2NTM3ODI3YTE0YTBmYjI0YjZkNDllNw'
--- scheduler/scheduler-YTQwNzI3MjliNzA5NjdmNjg0YTlmNTI1MDI3NjMyYjI1MDk5OTNlODlhYzcyODJhZDZjNjExOTc4OWIxOWI3Nw.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/curl -k 'https://localhost/suhu-app/get-suhu/YTQwNzI3MjliNzA5NjdmNjg0YTlmNTI1MDI3NjMyYjI1MDk5OTNlODlhYzcyODJhZDZjNjExOTc4OWIxOWI3Nw'
