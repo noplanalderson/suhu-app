6344ed245426fb48c308d66d828a6401cd1a7796
noplanalderson <noplanalderson@gmail.com>
2021-02-13 20:40:47 +0700

    Add Scheduler Script Directory

---
+++ scheduler/scheduler-MDRmODNlMzQxODlkMWZhYTk1NWU4MjcwODYzNWYzNzQzYzlhYTBmOTU0ZDk5ZTVkN2RjZjkzMTRlNGUyNDk4Yg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/curl -k 'https://localhost/suhu-app/get-temp/MDRmODNlMzQxODlkMWZhYTk1NWU4MjcwODYzNWYzNzQzYzlhYTBmOTU0ZDk5ZTVkN2RjZjkzMTRlNGUyNDk4Yg'
---
+++ scheduler/scheduler-MjJmNGFjMGUwYzYwZGI0Y2ZmODkzMGVjZmY5ZDk5ZDA2Y2VmN2JlZWRkYThjMmUwN2VmYTdhNzFmNDIxMDMyZA.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/curl -k 'https://localhost/suhu-app/get-temp/MjJmNGFjMGUwYzYwZGI0Y2ZmODkzMGVjZmY5ZDk5ZDA2Y2VmN2JlZWRkYThjMmUwN2VmYTdhNzFmNDIxMDMyZA'
