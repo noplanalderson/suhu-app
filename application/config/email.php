1a4648a733d392ee3264cece4df4924ea9c23e66
noplanalderson <noplanalderson@gmail.com>
2021-02-15 22:38:15 +0700

    Add SMTP Configuration File

---
+++ application/config/email.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = array(
+    'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
+    'smtp_host' => '', 
+    'smtp_port' => ,
+    'smtp_user' => '',
+    'smtp_pass' => '',
+    'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
+    'mailtype' => 'text/plan', //plaintext 'text' mails or 'html'
+    'smtp_timeout' => '4', //in seconds
+    'charset' => 'utf-8',
+    'wordwrap' => TRUE
+);
