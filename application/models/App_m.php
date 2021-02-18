9fb8a835cd8e96534bc5560f760936b0683aa43f
noplanalderson <noplanalderson@gmail.com>
2021-02-18 15:22:11 +0700

    Add Method Update Setting, Remove Remember Me Feature

--- application/models/App_m.php
+++ application/models/App_m.php
@@ -46,30 +46,20 @@ class App_m extends CI_Model {
 
 	public function getUserProfile()
 	{
-		$this->db->select("a.user_name, a.user_picture, a.user_realname, FROM_UNIXTIME(b.login_date) AS last_login, INET6_NTOA(b.last_ip) AS last_ip, c.type_name");
-		$this->db->join('tb_login_data b', 'a.user_id = b.user_id', 'left');
-		$this->db->join('tb_user_type c', 'a.type_id = c.type_id', 'inner');
+		$this->db->select("a.user_name, a.user_picture, a.user_realname, b.type_name");
+		$this->db->join('tb_user_type b', 'a.type_id = b.type_id', 'inner');
 		$this->db->where('a.user_id', $this->session->userdata('uid'));
-		$this->db->order_by('b.login_date', 'desc');
-		return $this->db->get('tb_user a', 1,1)->row();
-	}
-
-	public function updateLastActivity()
-	{
-		$data = array(
-			'date_login' => $this->session->userdata('time'),
-			'last_ip' => inet_pton(get_real_ip()),
-			'cookie_expire' => NULL,
-			'cookie_value' => NULL
-		);
-
-		$this->db->where('user_id', $this->session->userdata('uid'));
-		$this->db->update('tb_user', $data);
+		return $this->db->get('tb_user a')->row();
 	}
 
 	public function getAppSetting()
 	{
 		return $this->db->get('tb_app_setting', 1)->row();
+	}
+
+	public function updateSettings($settingData)
+	{
+		return $this->db->update('tb_app_setting', $settingData) ? true : false;
 	}
 }
 
